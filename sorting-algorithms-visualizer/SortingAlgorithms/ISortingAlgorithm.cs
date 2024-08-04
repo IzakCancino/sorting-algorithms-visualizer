7e545e85b52a5f31818ccbb54e6adbf11a4a9be6
IzakCancino <84246483+IzakCancino@users.noreply.github.com>
2024-08-03 19:46:53 -0500

    Documentation of ISortingAlgorithm interface

--- sorting-algorithms-visualizer/SortingAlgorithms/ISortingAlgorithm.cs
+++ sorting-algorithms-visualizer/SortingAlgorithms/ISortingAlgorithm.cs
@@ -13,6 +13,12 @@ namespace sorting_algorithms_visualizer.SortingAlgorithms
         public string TimeComplexity { get; }
         public string SpaceComplexity { get; }
 
+        /// <summary>
+        /// Sort a specific list of <c>RectangleNode</c> type.
+        /// </summary>
+        /// <param name="list">List of nodes to sort.</param>
+        /// <param name="log">Log text box used to print information about the process.</param>
+        /// <param name="cancellationToken">Token used in case of the process is canceled by another one.</param>
         public Task Sort(List<RectangleNode> list, RichTextBox log, CancellationToken cancellationToken);
     }
 }
